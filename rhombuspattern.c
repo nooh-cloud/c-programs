fd823fb27aed808ce13cb76b7cb66a1c79c5abb0
abdulla Nooh <abdullanooh552@gmail.com>
2024-09-01 22:15:03 +0530

    Add files via upload

--- rhombuspattern.c
+++ rhombuspattern.c
@@ -5,14 +5,19 @@ int main(){
     printf("Enter the number of rows:");
     scanf("%d",&rows);
 
-    for ( i = rows; i >= 1; i--) {
+    for ( i = 1; i <= rows ; i++){
+
         for ( j = 1; j <= rows - i; j++){
+
             printf(" ");
         }
-        for ( j = 1; j <= 2 * i - 1; j++){
+        for ( j = 1; j <= rows; j++)
+        {
             printf("*");
         }
         printf("\n");
+        
     }
     return 0;
+
 }
